7b5fc6d4b35d123ff1de6b45aa04f4035973b245
philliplegate <150298481+philliplegate@users.noreply.github.com>
2023-11-08 16:02:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents new health Diner')
+streamlit.title('My Parents New Health Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blkueberry Oatmeal')
