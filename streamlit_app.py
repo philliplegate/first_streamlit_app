91545bc8210f4be177cd7cfab92a849a4a2471b2
philliplegate <150298481+philliplegate@users.noreply.github.com>
2023-11-13 13:08:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+try:
+      fruit_choice=streamlit.text_input('What fruit would you like information about')
+      if not fruit_choice:
+         streamlit.eror("Please select a fruit to get information.")
+      else:
+         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response.json)
 
+except URLError as e:
+   streamlist.error()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
